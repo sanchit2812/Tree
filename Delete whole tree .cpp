948262c0800d371e7071c2018a687408be08251b
sanchit garg <sanchitjssaten@gmail.com>
2018-08-24 01:00:49 +0530

    Update and rename delete tree .cpp to Delete whole tree .cpp

--- Delete whole tree .cpp
+++ Delete whole tree .cpp
@@ -1,3 +1,5 @@
+# Function to delte a tree
+
 void _deleteTree(struct node* node)
 {
     if (node == NULL) return;
@@ -6,8 +8,10 @@ void _deleteTree(struct node* node)
     free(node);
 }
 
+# MAIN Function for deleting a tree 
 void deleteTree(struct node* *node_ref)
 {
   _deleteTree(*node_ref);
+  # This is used because sometimes the root node should be equal to NULL
   *node_ref = NULL;
 }
